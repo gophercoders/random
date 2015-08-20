ce778b67ba0b07a3ae210c67ba03c46d02114060
Owen Waller <o.waller@kulawe.com>
2015-08-20 18:02:26 +0100

    Completely rewrite the package to work with negative numbers
    The package has been completely rewritten to return both positive
    and negative numbers if the value of min passed to
    GetRandomNumberInRange is less than zero.
    
    The values of both min and max will be included in the range of numbers
    that GetRandomNumberInRange can return.

--- random.go
+++ random.go
@@ -1,3 +1,14 @@
+// Copyright 2015 Kulawe Limited. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package random providesa simple way to generate randon number, both
+// positive and negative within a range.
+//
+// The functions provided by this module are not crytographically safe as
+// the package uses the pseudo-random number generator from the math.rand
+// package. The random number generator is seeded with the time in nanoseconds
+// that this package is first initialised.
 package random
 
 import (
@@ -9,13 +20,54 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
+// GetRandomNumberInRange returns an int in the range min to max (inclusive).
+// In other words the values of both min and max are included in the range and
+// can be returned by the function. The function can return negative numbers if
+// min is less than zero, so a range between -12 and 1 can return the numbers
+// -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1.
+//
+// The function will panic is max < min or if max == min.
 func GetRandomNumberInRange(min, max int) int {
 	var n int
 	var r int
+	// max < min is bad
 	if max < min {
-		panic("Max < min")
+		panic("max < min")
 	}
-	r = (max - min) + 1
-	n = rand.Intn(r)
-	return n + min
+	//max == min is also bad
+	if max == min {
+		panic("max == min")
+	}
+	// looking for a number in the range zero to max (where max is positive) were
+	// both zero amd max are included in the range.
+	if min == 0 && max > 0 {
+		n = rand.Intn(max + 1) // gives range of 0..max
+		return n
+	}
+	// looking for a number in the range min (were min is negative) to zero were
+	// both min and zero are in the range
+	if max == 0 && min < 0 {
+		n = rand.Intn((min - 1) * -1) // gives range 0..abs(min)+1. rand.Intn requires a postive number
+		return n * -1                 // the range consists of only or negative numbers so negate the answer
+	}
+	if min < 0 {
+		if max < 0 {
+			// looking for a number in the range min to max were both numbers are negative
+			// and both numbers are included in the range.
+			r = (max - min) - max // can be negative
+			if r < 0 {            // if it is negative make it positive
+				r = r * -1
+			}
+		} else {
+			// looking for a number in the range min to max were min is negative
+			// and max is positive and both numbers are included in the range.
+			r = (max - min) + max // can never be negative
+		}
+	} else if min > 0 {
+		// looking for a number in the range min to max where both min and max
+		// are positive and both numbers are included in the range.
+		r = (max - min) + min
+	}
+	n = rand.Intn(r) // gives a number in the range 0..r-1
+	return (n + min) // gives a number in the range n..r-1+min. When min is negative min is subtracted not added.
 }
--- random_test.go
+++ random_test.go
@@ -1,24 +1,31 @@
 package random
 
-import (
-	"math/rand"
-	"testing"
-	"time"
-)
+import "testing"
 
-func init() {
-	rand.Seed(time.Now().UnixNano())
+var randomTestResults = []struct {
+	min int
+	max int
+}{
+	{0, 12},
+	{-12, 0},
+	{1, 12},
+	{-12, -1},
+	{-12, 1},
 }
 
 func TestGetRandomNumberInRange(t *testing.T) {
 	var n int
-	for i := 0; i < 100000000; i++ {
-		n = GetRandomNumberInRange(1, 12)
-		if n < 1 {
-			t.Fatal("Got a unexpected zero")
-		}
-		if n > 12 {
-			t.Fatal("Got an number larger than 12")
+	for _, td := range randomTestResults {
+		// loop 100 million times - to make sure the random-ness averages out
+		// this will take a long time to run.
+		for i := 0; i < 100000000; i++ {
+			n = GetRandomNumberInRange(td.min, td.max)
+			if n < td.min {
+				t.Fatalf("Random Number to small. Got %d but min is %d\n", n, td.min)
+			}
+			if n > td.max {
+				t.Fatalf("Random Number to big. Got %d but max is %d\n", n, td.max)
+			}
 		}
 	}
 }
